1f37eebc1c05ff81ec638be2bee27f7b98b7c010
muwattah <elabd214o@gmail.com>
2026-09-15 10:30:06 +0200

    Add CoolProp saturation tables, subcool grids, and grid loader (part 1/4)

---
+++ data_grids_loader.js
@@ -0,0 +1,5 @@
+// Assemble grids from parts (window globals for cross-script access)
+window.SH_GRID = window.SH_PART || {};
+window.SC_GRID = window.SC_PART || {};
+var SH_GRID = window.SH_GRID;
+var SC_GRID = window.SC_GRID;
