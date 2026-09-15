5e82b5b44408ca13c48a0447186749e545bf7541
muwattah <elabd214o@gmail.com>
2026-09-15 10:56:53 +0200

    Add REF loader + improved grids loader

--- data_grids_loader.js
+++ data_grids_loader.js
@@ -1,5 +1,17 @@
-// Assemble grids from parts (window globals for cross-script access)
-window.SH_GRID = window.SH_PART || {};
-window.SC_GRID = window.SC_PART || {};
+// Assemble SH/SC grids from window.SH_PART / window.SC_PART
+// Scripts order: sh_sparse_a, sh_sparse_b, sc_R*, then this loader
+window.SH_PART = window.SH_PART || {};
+window.SC_PART = window.SC_PART || {};
+window.SH_GRID = window.SH_PART;
+window.SC_GRID = window.SC_PART;
 var SH_GRID = window.SH_GRID;
 var SC_GRID = window.SC_GRID;
+// Integrity log
+(function(){
+  var refs = ['R134a','R513A','R410A','R32'];
+  var shOk = refs.every(function(r){ return SH_GRID[r] && SH_GRID[r].length; });
+  var scOk = refs.every(function(r){ return SC_GRID[r] && SC_GRID[r].length; });
+  console.log('Grids loaded: SH=' + (shOk?'OK':'MISSING') + ' SC=' + (scOk?'OK':'MISSING'),
+    'SH keys=' + Object.keys(SH_GRID).join(','),
+    'SC keys=' + Object.keys(SC_GRID).join(','));
+})();
---
+++ data_ref_loader.js
@@ -0,0 +1,4 @@
+// Assemble REF from parts
+window.REF_PART = window.REF_PART || [];
+var REF = window.REF_PART;
+console.log('REF loaded:', REF.length, 'tests');
